7f07551b1a5d9d65e29062158a08a263ab75efd6
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-08-19 06:11:08 -0600

    worked on the terminal tool

--- tool.py
+++ tool.py
@@ -4,5 +4,6 @@ def main(argv):
         return BAD_ARGS
     dbname, verb, key, value = (argv[1:] + [None]) [:4]
     
-    if verb not in {'get', 'set', 'delete'}:
+    if verb not in {'get', 'set', 'delete'}: #getters and setters for verb
         usage()
+        return BAD_VERB 
