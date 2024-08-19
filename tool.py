5a391c9100df0ff7a2ce365879c7b6e777a28b44
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-08-19 05:32:46 -0600

    first commit

---
+++ tool.py
@@ -0,0 +1,8 @@
+def main(argv):
+    if not (4 <= len(argv) <= 5):
+        usage()
+        return BAD_ARGS
+    dbname, verb, key, value = (argv[1:] + [None]) [:4]
+    
+    if verb not in {'get', 'set', 'delete'}:
+        usage()
