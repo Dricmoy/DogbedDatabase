4a8f86d8bc59686320d33fec22446a67f9073d5a
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-08-23 05:37:55 -0600

    dbdb storage interface and logical abstraction

--- tool.py
+++ tool.py
@@ -2,13 +2,21 @@ def main(argv):
     if not (4 <= len(argv) <= 5):
         usage()
         return BAD_ARGS
-    dbname, verb, key, value = (argv[1:] + [None]) [:4]
-    
-    if verb not in {'get', 'set', 'delete'}: #getters and setters for verb
+    dbname, verb, key, value = (argv[1:] + [None])[:4]
+    if verb not in {'get', 'set', 'delete'}:
         usage()
         return BAD_VERB
-    
-    db = dbdb.connect(dbname)          #connect to the db 
+    db = dbdb.connect(dbname)          
     try:
         if verb == 'get':
-            sys.stdout.write 
+            sys.stdout.write(db[key])  
+        elif verb == 'set':
+            db[key] = value
+            db.commit()
+        else:
+            del db[key]
+            db.commit()
+    except KeyError:
+        print("Key not found", file=sys.stderr)
+        return BAD_KEY
+    return OK
